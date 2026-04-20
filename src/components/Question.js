832cf1e6f000ad77a84a9e37dc84a8c8b396ee44
Dev <devmaheswari2017@gmail.com>
2026-04-20 12:26:27 +0530

    working on heading fixes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42d6f45e3a91b32b1373a1e6d6c8c5b9388b3bbbe8dbbd273ba82c13896f7a7a
-size 8528
+oid sha256:9beca37bd4887198674f01c5176694e0563885aeff86ecfe0f26713fb8d732e4
+size 8603
