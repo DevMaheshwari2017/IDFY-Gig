9083dc1ba723444a09c6c52cfc4375962f1a5ed0
Dev <devmaheswari2017@gmail.com>
2026-04-20 17:42:32 +0530

    added sticky + content change on scroll functionality in "The story behind" & "E-commerce" section

--- src/components/Risk.js
+++ src/components/Risk.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cf29abc890ba115f7481c2242061fa6079d43e938cd746340184a5454cdf116
-size 14072
+oid sha256:21366c7af5b54f75988cc2da7faa322f36b4afb01a0efb4ca4258208d63277af
+size 22694
